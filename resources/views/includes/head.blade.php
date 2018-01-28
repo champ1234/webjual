a94356bc755a8f6011443907abf16a6e6eb31343
champ1234 <www.waisefajar@gmail.com>
2018-01-28 08:21:18 +0700

    first commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<title></title>
+    	<meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<!--// Stylesheet //-->
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/normalize.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/bootstrap.min.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/font-awesome.min.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/themify-icons.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/flag-icon.min.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/cs-skin-elastic.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/style.css')}}">
+<link rel="stylesheet"  href="{{URL::asset('assets/assets/ccs/lib/vector-map/jqvmap.min.css')}}">
+<!-- <link rel="stylesheet"  href="{{URL::asset('assets/assets/css/bootstrap-select.less')}}"> --> 
+
+<link rel="apple-touch-icon" href="apple-icon.png">
+<link rel="shortcut icon" href="{{URL::asset('assets/favicon.ico')}}">
+
+
+
